d8eca321f8d7daee4ec8dae2bed418af13fd3e4a
astakh <v.astakhov@gmail.com>
2022-06-25 07:04:43 +0300

    multiplier

--- arbbot2.js
+++ arbbot2.js
@@ -136,7 +136,8 @@ async function handleBots(bot) {
     if (bot.stage == 1) {
         if (tradeAmount > 0) {
             console.log(`${bot.name}: placing orders`)
-            let reverse = ''
+            let reverse = ''; let leftMultiplier = 1 / 1.005; let rightMultiplier = 1.005;
+            if (bot.waiting) { leftMultiplier = rightMultiplier; rightMultiplier = 1 / leftMultiplier; }
             bot.orderLeft  = placeOrder(bot.exchanges.ccxt.left,  bot.orderMask[bot.waiting].left.pair,  'limit', bot.orderMask[bot.waiting].left.direction,  tradeAmount, market.left.coin[bot.waiting])
             if (bot.waiting == 'sell') {reverse = 'buy'} else { reverse = 'sell'}
             bot.orderRight = placeOrder(bot.exchanges.ccxt.right, bot.orderMask[bot.waiting].right.pair, 'limit', bot.orderMask[bot.waiting].right.direction, tradeAmount, market.right.coin[reverse])
